44efb6801a69ecb0e77c70e308f21b616efcbb69
lucianosants <lucianola18@gmail.com>
2022-12-03 16:08:49 -0300

    feat: updated about page and style

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,33 @@
+import { StyledAbout } from '../../styles/SharedStyles';
+
 export default function About() {
 	return (
-		<div>
+		<StyledAbout>
 			<h3>About page</h3>
-		</div>
+
+			<h4>
+				This project is designed to consume the{' '}
+				<strong className='about__highlight'>github API</strong>.
+			</h4>
+
+			<p>
+				Its purpose is to be a profile page based on{' '}
+				<strong className='about__highlight'>Twitter</strong> layout
+				with data brought from your{' '}
+				<strong className='about__highlight'>Github</strong>.
+			</p>
+
+			<p>
+				Created by{' '}
+				<a
+					href='https://github.com/lucianosants'
+					target='_blank'
+					rel='noreferrer'
+				>
+					lucianosants
+				</a>
+			</p>
+			<p></p>
+		</StyledAbout>
 	);
 }
--- src/styles/SharedStyles.ts
+++ src/styles/SharedStyles.ts
@@ -7,4 +7,36 @@ const StyledHome = styled.section`
 	gap: 3rem;
 `;
 
-export { StyledHome };
+const StyledAbout = styled(StyledHome)`
+	background-color: ${({ theme }) => theme.colors.BG_L2};
+	border: 1px solid ${({ theme }) => theme.colors.BORDER};
+	border-radius: 12px;
+	padding: 2.5rem;
+
+	p {
+		color: ${({ theme }) => theme.colors.TEXT_L2};
+		line-height: 1.6;
+		font-size: 1.4rem;
+	}
+
+	.about__highlight {
+		padding: 0.5rem;
+		text-decoration: 2px solid underline;
+
+		&:hover {
+			color: var(--brand);
+		}
+	}
+
+	a {
+		color: var(--brand);
+		font-size: 1.2rem;
+
+		&:hover,
+		&:focus {
+			color: ${({ theme }) => theme.colors.TEXT_L2};
+		}
+	}
+`;
+
+export { StyledHome, StyledAbout };
