9317a24a887fe73780a209d65158e59c3a63f381
lucianosants <lucianola18@gmail.com>
2022-12-09 10:36:26 -0300

    feat: added app-messages

---
+++ src/data/app-messages.ts
@@ -0,0 +1,13 @@
+const messages = {
+	error: [
+		'Field must be filled',
+		'Field must be at least 3 characters long',
+		'User not found',
+	],
+	success: [
+		'user found. Welcome to TwiiHub',
+		'User successfully found on github',
+	],
+};
+
+export { messages };
